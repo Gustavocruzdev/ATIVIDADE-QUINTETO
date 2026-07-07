8fae0294b2ef8acb0fe32a85475cd2efd176ff22
GuilhermeSilva-dev <guilherme.s.souza7@ba.estudante.senai.br>
2026-07-07 20:04:28 -0300

    feat: Criando classes.

---
+++ projeto-aoe/projeto-aoe/src/main/java/com/example/projeto_aoe/ProjetoAoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_aoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAoeApplication.class, args);
+	}
+
+}
